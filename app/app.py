7d550add966122c3ab64a7729d6f625ec6cc4cc6
ŁW <lukasz.wronski@gorrion.io>
2020-05-30 18:35:11 +0200

    return code fix

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, json
 from os import listdir
 from time import time
 from pathlib import Path
@@ -17,10 +17,9 @@ def root():
 def post():
     data = request.get_data()
     Path(DATA_PATH, str(time())).write_text(data.decode())
-    return 201
+    return '', 201
 
 @app.route('/stats', methods=["GET"])
-
 def stats_login():
     return render_template('stats_login.html', error = request.args.get('error') or "")
 
